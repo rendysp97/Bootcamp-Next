f49a09a9cffb377d28862fef4e39e63acc91e01a
rendysyahputra <rendysyahputra33@gmail.com>
2024-06-18 03:35:34 +0700

    menambahkan tugas 6

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,8 @@ import Layout from "@/layout";
 export default function Home() {
   return (
     <>
-      <Layout>
-        <h1>Profile</h1>
-      </Layout>
-      <Layout>
-        <h1>Detail</h1>
+      <Layout metaTitle="Homescreen" metaDescription="Ini adalah homescreen">
+        <h1>Home</h1>
       </Layout>
     </>
   );

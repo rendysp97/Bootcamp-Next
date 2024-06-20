a33be17d397a04ece0059087e6bb3906e633e873
rendysyahputra <rendysyahputra33@gmail.com>
2024-06-20 23:54:41 +0700

    menambahkan tugas 9

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+// export default function handler(req, res) {
+//   res.status(200).json({ name: "John Doe" });
+// }
+
+export default async function getApi(req, res) {
+  let response = await (
+    await fetch("https://jsonplaceholder.typicode.com/todos")
+  ).json();
+
+  let title = response.map((item) => item.title);
+
+  res.status(200).json({ title });
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    fetch("api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("ini", res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <Layout metaTitle="Homescreen" metaDescription="Ini adalah homescreen">

49df3f4467382ceb7bedcc5a3eec936623ff17fd
rendysyahputra <rendysyahputra33@gmail.com>
2024-06-25 23:32:10 +0700

    menambahkan tugas 12

---
+++ pages/notes/add.js
@@ -0,0 +1,75 @@
+import dynamic from "next/dynamic";
+import {
+  Grid,
+  GridItem,
+  Card,
+  Heading,
+  Text,
+  Button,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const LayoutComponent = dynamic(() => import("@/layout"));
+
+export default function AddNotes() {
+  const router = useRouter();
+  const [notes, setNotes] = useState({
+    title: "",
+    description: "",
+  });
+
+  const HandleSubmit = async () => {
+    try {
+      const response = await fetch("https://service.pace-unv.cloud/api/notes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(notes),
+      });
+      const result = await response.json();
+      if (result?.success) {
+        router.push("/notes");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <LayoutComponent metaTitle="Notes">
+        <Card margin="5" padding="5">
+          <Heading>Add Notes</Heading>
+          <Grid gap="5">
+            <GridItem>
+              <Text>Title</Text>
+              <Input
+                type="text"
+                onChange={(event) =>
+                  setNotes({ ...notes, title: event.target.value })
+                }
+              />
+            </GridItem>
+            <GridItem>
+              <Text>Description</Text>
+              <Textarea
+                onChange={(event) =>
+                  setNotes({ ...notes, description: event.target.value })
+                }
+              />
+            </GridItem>
+            <GridItem>
+              <Button onClick={() => HandleSubmit()} colorScheme="blue">
+                Submit
+              </Button>
+            </GridItem>
+          </Grid>
+        </Card>
+      </LayoutComponent>
+    </>
+  );
+}
--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,28 +1,86 @@
-import Layout from "@/layout";
-import Link from "next/link";
+import dynamic from "next/dynamic";
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Card,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  Heading,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
+const LayoutComponent = dynamic(() => import("@/layout"));
 
-export default function notes({ data }) {
-  console.log(data);
+export default function Notes() {
+  const router = useRouter();
+  const [notes, setNotes] = useState();
+
+  useEffect(() => {
+    async function fetchingData() {
+      const res = await (
+        await fetch("https://service.pace-unv.cloud/api/notes")
+      ).json();
+
+      setNotes(res);
+    }
+    fetchingData();
+  }, []);
+
   return (
-    <Layout>
-      {data.data.map((item) => {
-        return (
-          <div key={item.id}>
-            <Link href={`/notes/${item.id}`}>{item.title}</Link>
-          </div>
-        );
-      })}
-    </Layout>
+    <>
+      <LayoutComponent metaTitle="Notes">
+        <Box padding="5">
+          <Flex justifyContent="end">
+            <Button
+              colorScheme="blue"
+              onClick={() => router.push("/notes/add")}
+            >
+              Add Notes
+            </Button>
+          </Flex>
+          <Flex>
+            <Grid templateColumns="repeat(3, 1fr)" gap={5}>
+              {notes?.data?.map(({title,description}) => (
+                <GridItem>
+                  <Card>
+                    <CardHeader>
+                      <Heading>{title}</Heading>
+                    </CardHeader>
+                    <CardBody>
+                      <Text>{description}</Text>
+                    </CardBody>
+                    <CardFooter justify="space-between" flexWrap="wrap">
+                      <Button
+                        onClick={() => router.push(`/notes/edit/${item?.id}`)}
+                        flex="1"
+                        variant="ghost"
+                      >
+                        Edit
+                      </Button>
+                      <Button flex="1" colorScheme="red">
+                        Delete
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                </GridItem>
+              ))}
+            </Grid>
+          </Flex>
+        </Box>
+      </LayoutComponent>
+    </>
   );
 }
 
+// export async function getStaticProps() {
+//   const res = await fetch("https://service.pace-unv.cloud/api/notes");
+//   const data = await res.json();
 
-
-export async function getStaticProps() {
-  const res = await fetch("https://service.pace-unv.cloud/api/notes");
-  const data = await res.json();
-
-  return { props: { data }, revalidate: 5 };
-}
-
+//   return { props: { data }, revalidate: 5 };
+// }
